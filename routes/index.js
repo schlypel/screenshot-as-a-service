ace1a3842338560660530a19e25911c8104e84f1
Schlypel <schlypel@users.noreply.github.com>
2014-08-20 14:32:02 +0200

    fixed bug in case of 404 and adjusted indentation

--- routes/index.js
+++ routes/index.js
@@ -6,132 +6,131 @@ var request = require('request');
 var gm = require('gm');
 
 module.exports = function(app, useCors) {
-  var rasterizerService = app.settings.rasterizerService;
-  var fileCleanerService = app.settings.fileCleanerService;
+    var rasterizerService = app.settings.rasterizerService;
+    var fileCleanerService = app.settings.fileCleanerService;
 
-  // routes
-  app.get('/', function(req, res, next) {
-    if (!req.param('url', false)) {
-      return res.redirect('/usage.html');
-    }
+    // routes
+    app.get('/', function(req, res, next) {
+        if (!req.param('url', false)) {
+            return res.redirect('/usage.html');
+        }
 
-    var url = utils.url(req.param('url'));
-    // required options
-    var options = {
-      uri: 'http://localhost:' + rasterizerService.getPort() + '/',
-      headers: { url: url, zoom: 1 }
-    };
-    ['width', 'height', 'zoom', 'clipRect', 'resizeWidth', 'resizeHeight', 'javascriptEnabled', 'loadImages', 'localToRemoteUrlAccessEnabled', 'userAgent', 'userName', 'password', 'delay'].forEach(function(name) {
-      if (req.param(name, false)) options.headers[name] = req.param(name);
+        var url = utils.url(req.param('url'));
+        // required options
+        var options = {
+            uri: 'http://localhost:' + rasterizerService.getPort() + '/',
+            headers: { url: url, zoom: 1 }
+        };
+        ['width', 'height', 'zoom', 'clipRect', 'resizeWidth', 'resizeHeight', 'javascriptEnabled', 'loadImages', 'localToRemoteUrlAccessEnabled', 'userAgent', 'userName', 'password', 'delay'].forEach(function(name) {
+            if (req.param(name, false)) options.headers[name] = req.param(name);
+        });
+
+        var filename = 'screenshot_' + utils.md5(url + JSON.stringify(options)) + '.png';
+        options.headers.filename = filename;
+
+        var filePath = join(rasterizerService.getPath(), filename);
+
+        var callbackUrl = req.param('callback', false) ? utils.url(req.param('callback')) : false;
+
+        if (fs.existsSync(filePath)) {
+            console.log('Request for %s - Found in cache', url);
+            processImageUsingCache(filePath, res, callbackUrl, function(err) { if (err) next(err); });
+            return;
+        }
+        console.log('Request for %s - Rasterizing it', url);
+        processImageUsingRasterizer(options, filePath, res, callbackUrl, function(err) { if(err) next(err); });
     });
 
-    var filename = 'screenshot_' + utils.md5(url + JSON.stringify(options)) + '.png';
-    options.headers.filename = filename;
-
-    var filePath = join(rasterizerService.getPath(), filename);
-
-    var callbackUrl = req.param('callback', false) ? utils.url(req.param('callback')) : false;
-
-    if (fs.existsSync(filePath)) {
-      console.log('Request for %s - Found in cache', url);
-      processImageUsingCache(filePath, res, callbackUrl, function(err) { if (err) next(err); });
-      return;
-    }
-    console.log('Request for %s - Rasterizing it', url);
-    processImageUsingRasterizer(options, filePath, res, callbackUrl, function(err) { if(err) next(err); });
-  });
-
-  app.get('*', function(req, res, next) {
-    // for backwards compatibility, try redirecting to the main route if the request looks like /www.google.com
-    res.redirect('/?url=' + req.url.substring(1));
-  });
-
-  // bits of logic
-  var processImageUsingCache = function(filePath, res, url, callback) {
-    if (url) {
-      // asynchronous
-      res.send('Will post screenshot to ' + url + ' when processed');
-      postImageToUrl(filePath, url, callback);
-    } else {
-      // synchronous
-      sendImageInResponse(filePath, res, callback);
-    }
-  }
-
-  var processImageUsingRasterizer = function(rasterizerOptions, filePath, res, url, callback) {
-    if (url) {
-      // asynchronous
-      res.send('Will post screenshot to ' + url + ' when processed');
-      callRasterizer(rasterizerOptions, function(error) {
-        if (error) return callback(error);
-        postImageToUrl(filePath, url, callback);
-      });
-    } else {
-      // synchronous
-      callRasterizer(rasterizerOptions, function(error) {
-        if (error) return callback(error);
-        sendImageInResponse(filePath, res, callback);
-      });
-    }
-  }
-
-  var callRasterizer = function(rasterizerOptions, callback) {
-    request.get(rasterizerOptions, function(error, response, body) {
-      if (error || response.statusCode != 200) {
-        console.log('Error while requesting the rasterizer: %s', error.message);
-        rasterizerService.restartService();
-        return callback(new Error(body));
-      }
-      var filePath = join(rasterizerService.getPath(), rasterizerOptions.headers.filename);
-      if (rasterizerOptions.headers.resizeWidth &&
-		  rasterizerOptions.headers.resizeWidth > 1 &&
-		  rasterizerOptions.headers.resizeHeight &&
-		  rasterizerOptions.headers.resizeHeight > 1){
-		gm(filePath)
-			.resize(rasterizerOptions.headers.resizeWidth,rasterizerOptions.headers.resizeHeight)
-			.write(filePath + '.tmp', function (error) {
-				if (error) {
-					console.log('Error - ', error);
-				}else{
-					fs.writeFileSync(filePath, fs.readFileSync(filePath + '.tmp'));
-					fs.unlink(filePath + '.tmp', function(err){
-					if(err){
-						console.log(err);
-					}
-					callback(null);
-				});
-			}
-		});
-	  }
+    app.get('*', function(req, res, next) {
+        // for backwards compatibility, try redirecting to the main route if the request looks like /www.google.com
+        res.redirect('/?url=' + req.url.substring(1));
     });
-  }
 
-  var postImageToUrl = function(imagePath, url, callback) {
-    console.log('Streaming image to %s', url);
-    var fileStream = fs.createReadStream(imagePath);
-    fileStream.on('end', function() {
-      fileCleanerService.addFile(imagePath);
-    });
-    fileStream.on('error', function(err){
-      console.log('Error while reading file: %s', err.message);
-      callback(err);
-    });
-    fileStream.pipe(request.post(url, function(err) {
-      if (err) console.log('Error while streaming screenshot: %s', err);
-      callback(err);
-    }));
-  }
-
-  var sendImageInResponse = function(imagePath, res, callback) {
-    console.log('Sending image in response');
-    if (useCors) {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Access-Control-Expose-Headers", "Content-Type");
+    // bits of logic
+    var processImageUsingCache = function(filePath, res, url, callback) {
+        if (url) {
+            // asynchronous
+            res.send('Will post screenshot to ' + url + ' when processed');
+            postImageToUrl(filePath, url, callback);
+        } else {
+            // synchronous
+            sendImageInResponse(filePath, res, callback);
+        }
     }
-    res.sendfile(imagePath, function(err) {
-      fileCleanerService.addFile(imagePath);
-      callback(err);
-    });
-  }
-
+    var processImageUsingRasterizer = function(rasterizerOptions, filePath, res, url, callback) {
+        if (url) {
+            // asynchronous
+            res.send('Will post screenshot to ' + url + ' when processed');
+            callRasterizer(rasterizerOptions, function(error) {
+                if (error) return callback(error);
+                postImageToUrl(filePath, url, callback);
+            });
+        } else {
+            // synchronous
+            callRasterizer(rasterizerOptions, function(error) {
+                if (error) return callback(error);
+                sendImageInResponse(filePath, res, callback);
+            });
+        }
+    }
+    var callRasterizer = function(rasterizerOptions, callback) {
+        request.get(rasterizerOptions, function(error, response, body) {
+            if (error || response.statusCode != 200) {
+                console.log('Error while requesting the rasterizer: %s', error.message);
+                rasterizerService.restartService();
+                return callback(new Error(body));
+            }
+            var filePath = join(rasterizerService.getPath(), rasterizerOptions.headers.filename);
+            if (fs.existsSync(filePath) &&
+                rasterizerOptions.headers.resizeWidth &&
+                rasterizerOptions.headers.resizeWidth > 1 &&
+                rasterizerOptions.headers.resizeHeight &&
+                rasterizerOptions.headers.resizeHeight > 1){
+                gm(filePath)
+                .resize(rasterizerOptions.headers.resizeWidth,rasterizerOptions.headers.resizeHeight)
+                .write(filePath + '.tmp', function (error) {
+                    if (error) {
+                        console.log('Error - ', error);
+                        callback(null);
+                    }else{
+                        fs.writeFileSync(filePath, fs.readFileSync(filePath + '.tmp'));
+                        fs.unlink(filePath + '.tmp', function(err){
+                            if(err){
+                                console.log(err);
+                            }
+                            callback(null);
+                        });
+                    }
+                });
+            }else{
+                callback(null);
+            }
+        });
+    }
+    var postImageToUrl = function(imagePath, url, callback) {
+        console.log('Streaming image to %s', url);
+        var fileStream = fs.createReadStream(imagePath);
+        fileStream.on('end', function() {
+            fileCleanerService.addFile(imagePath);
+        });
+        fileStream.on('error', function(err){
+            console.log('Error while reading file: %s', err.message);
+            callback(err);
+        });
+        fileStream.pipe(request.post(url, function(err) {
+            if (err) console.log('Error while streaming screenshot: %s', err);
+            callback(err);
+        }));
+    }
+    var sendImageInResponse = function(imagePath, res, callback) {
+        console.log('Sending image in response');
+        if (useCors) {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Expose-Headers", "Content-Type");
+        }
+        res.sendfile(imagePath, function(err) {
+            fileCleanerService.addFile(imagePath);
+            callback(err);
+        });
+    }
 };
